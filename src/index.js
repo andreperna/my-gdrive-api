838579f41eb2979943673fa8672237522b938221
andreperna <andreperna@gmail.com>
2023-04-25 15:11:55 -0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,2 @@
-
-// const id = await gDriveFunctions.getSheetId("todos", "tbl_users");
-
-const spreadsheetId = "1V2PHqeBW7QRJccYMTEtr8oYzNCU0MdruwHXxVAlge6o";
-
-async function debug(func) {
-  console.log(await func);
-}
-
-
-import { gDriveFunctions } from "./gDrive/gDriveFunctions.js";
-import { gSheetFunctions } from "./gSheets/gSheetsFunctions.js";
-
-// debug(await gDriveFunctions.getFolders())
-// debug(await gDriveFunctions.getFolderId("todos"))
-// debug(await gDriveFunctions.getFolderId("errado"))
-// debug(await gDriveFunctions.getSheets("todos"))
-// debug(await gDriveFunctions.getSheets("todo"))
-// debug(await gDriveFunctions.getSheetId("todos", "tbl_users"))
-
-// debug(await gSheetFunctions.getValuesNotNull(spreadsheetId))
-debug(await gSheetFunctions.getValuesNotNull(spreadsheetId))
+export { gDriveFunctions } from "./gDrive/gDriveFunctions.js"
+export { gSheetFunctions } from "./gSheets/gSheetsFunctions.js"
